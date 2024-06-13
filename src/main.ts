0c2e07a97a47004320018d1fe5cca01ba2536cb6
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2024-06-13 17:57:42 +0200

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { createPinia } from 'pinia'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+const pinia = createPinia()
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(pinia)
+
+app.use(vuetify)
+
+app.mount('#app')
